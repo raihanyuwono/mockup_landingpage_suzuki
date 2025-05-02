66bf9e85398cea911b0241dd7bdce730d004233c
Muhammad Raihan Wahyu Yuwono <raihanyuwono@hotmail.com>
2025-05-02 13:34:05 +0700

    edit fab-phone, add thank you page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Titan_One } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -12,9 +12,16 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const titanOne = Titan_One({
+  variable: "--font-titan-one",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
 export const metadata: Metadata = {
   title: "Suzuki 6.6 | Diskon Mobil Suzuki Terbaru",
-  description: "Hanya 6.6! Promo mobil Suzuki dengan potongan harga dan bonus menarik!",
+  description:
+    "Hanya 6.6! Promo mobil Suzuki dengan potongan harga dan bonus menarik!",
 };
 
 export default function RootLayout({
@@ -26,7 +33,7 @@ export default function RootLayout({
     <html lang="en">
       <head></head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${titanOne.variable} antialiased`}
       >
         {children}
       </body>
--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,7 +1,26 @@
 "use client";
 
+import Section from "@/components/Section";
+import styles from "@/styles/thankyou.module.css";
+import data from "@/data/thankyou.json";
+import ImageCustom from "@/components/ImageCustom";
+
 function ThankYou() {
-  return <>Thank You</>;
+  return (
+    <Section>
+      <div className={styles.container}>
+        <div className={styles.containerGreeting}>
+          <h1 className={styles.header}>{data.greeting}</h1>
+          <div className={styles.info}>{data.info}</div>
+        </div>
+        <ImageCustom
+          src="/images/suzuki_cars.webp"
+          alt="suzuki thank you"
+          height="h-40"
+        />
+      </div>
+    </Section>
+  );
 }
 
 export default ThankYou;
