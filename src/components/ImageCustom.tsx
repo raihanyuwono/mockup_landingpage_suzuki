0c30efbadba2124cc0dfed3600adf335bc06c483
Muhammad Raihan Wahyu Yuwono <raihanyuwono@hotmail.com>
2025-05-05 11:57:28 +0700

    Add button function

--- src/components/ImageCustom.tsx
+++ src/components/ImageCustom.tsx
@@ -5,11 +5,12 @@ interface Props {
   src: string;
   alt: string;
   styles: string;
+  onClick?: () => void;
 }
 
-function ImageCustom({ src, alt, styles="h-full w-full" }: Props) {
+function ImageCustom({ src, alt, styles="h-full w-full", onClick }: Props) {
   return (
-    <div className={`${style.imageContainer} ${styles}`}>
+    <div className={`${style.imageContainer} ${styles}`} onClick={onClick}>
       <Image
         className={style.images}
         src={src}
--- src/contents/PromoItem.tsx
+++ src/contents/PromoItem.tsx
@@ -3,13 +3,25 @@ import ImageCustom from "@/components/ImageCustom";
 import Section from "@/components/Section";
 import data from "@/data/promoItem.json";
 import styles from "@/styles/promoItem.module.css";
+import { useEffect, useRef } from "react";
 
 function PromoItem() {
+  const galleryScroll = useRef<null | HTMLDivElement>(null);
+
+  function onClick(direction: string) {
+      if (direction == "prev") galleryScroll.current!.scrollLeft -= 500;
+      if (direction == "next") galleryScroll.current!.scrollLeft += 500;
+  }
+
+  useEffect(() => {
+    if (galleryScroll.current) galleryScroll.current.scrollLeft = 0;
+  }, []);
+
   return (
     <Section>
       <div className={styles.wrapper}>
         <h2 className={styles.header}>{data.header}</h2>
-        <div className={styles.gallery}>
+        <div ref={galleryScroll} className={styles.gallery}>
           {data.items.map((car, id) => (
             <CardGallery
               key={id}
@@ -23,11 +35,13 @@ function PromoItem() {
             src="/images/arrow.svg"
             alt="arrow left"
             styles={`${styles.arrow} ${styles.arrowL}`}
+            onClick={() => onClick("prev")}
           />
           <ImageCustom
             src="/images/arrow.svg"
             alt="arrow right"
             styles={`${styles.arrow} ${styles.arrowR}`}
+            onClick={() => onClick("next")}
           />
         </div>
       </div>
